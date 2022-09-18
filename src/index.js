b1e094611221f9882ecbe697d45404a9c4eb4341
Yasser Ehaimer <yehaimer@Yassers-MacBook-Pro.local>
2022-09-18 16:08:53 -0400

    Add people from firestore

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, doc } from 'firebase/firestore';
+import { getFirestore, collection, doc, getDocs} from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDuzvlK2q939LGuhDXP2cLyxDqW81uQE9M",
@@ -18,6 +18,7 @@ const db = getFirestore(app);
 
 document.addEventListener('DOMContentLoaded', () => {
   //set up the dom events
+  getPeople()
   document
     .getElementById('btnCancelPerson')
     .addEventListener('click', hideOverlay);
@@ -45,4 +46,35 @@ function showOverlay(ev) {
   const id = ev.target.id === 'btnAddPerson' ? 'dlgPerson' : 'dlgIdea';
   //TODO: check that person is selected before adding an idea
   document.getElementById(id).classList.add('active');
+}
+
+const people = []; //to hold all the people from the collection
+
+async function getPeople(){
+  //call this from DOMContentLoaded init function 
+  //the db variable is the one created by the getFirestore(app) call.
+  const querySnapshot = await getDocs(collection(db, 'people'));
+  querySnapshot.forEach((doc) => {
+    //every `doc` object has a `id` property that holds the `_id` value from Firestore.
+    //every `doc` object has a doc() method that gives you a JS object with all the properties
+    const data = doc.data();
+    const id = doc.id;
+    people.push({id, ...data});
+  });
+  buildPeople(people);
+}
+
+function buildPeople(people){
+  //build the HTML
+  let ul = document.querySelector('ul.person-list');
+  let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  //replace the old ul contents with the new.
+  ul.innerHTML = people.map(person=>{
+    const dob = `${months[person['birth-month']-1]} ${person['birth-day']}`;
+    //Use the number of the birth-month less 1 as the index for the months array
+    return `<li data-id="${person.id}" class="person">
+            <p class="name">${person.name}</p>
+            <p class="dob">${dob}</p>
+          </li>`;
+  }).join('');
 }
