b9daaefb1150ff1a04441172387a6e74f32ae725
Yasser Ehaimer <yehaimer@Yassers-MacBook-Pro.local>
2022-09-24 15:13:59 -0400

    all required features work

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, doc, getDocs, query, where, addDoc, deleteDoc, onSnapshot, updateDoc, getDoc} from 'firebase/firestore';
 let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-let giftId
+let giftId // to pass gift-id to edit and delete functions
 
 const firebaseConfig = {
   apiKey: "AIzaSyDuzvlK2q939LGuhDXP2cLyxDqW81uQE9M",
@@ -12,10 +12,8 @@ const firebaseConfig = {
   appId: "1:1082424702054:web:85bda4155ca7159e8d69a7"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// get a reference to the database
-const db = getFirestore(app);
+const app = initializeApp(firebaseConfig); // Initialize Firebase
+const db = getFirestore(app);// get a reference to the database
 
 document.addEventListener('DOMContentLoaded', () => {
   getPeople()
@@ -35,7 +33,6 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function hideOverlay(ev) {
-
   ev.preventDefault();
   document.querySelector('.overlay').classList.remove('active');
   document
@@ -46,42 +43,33 @@ function hideOverlay(ev) {
 function showOverlay(ev, giftId) {
   ev.preventDefault();
   document.querySelector('.overlay').classList.add('active');
-
   let button
   switch (ev.target.classList.value) {
     case 'btnAddPerson':
       button = 'dlgPerson'
       break;
-
     case 'btnEditPerson':
       button = 'dlgPersonEdit'
       break;
-
     case 'btnDeletePerson':
       button = 'dlgPersonDelete'
       break;
-
     case 'btnAddIdea':
       button = 'dlgIdea'
       break;
-
     case 'btnEditIdea':
       button = 'dlgIdeaEdit'
       break;
-
     case 'btnDeleteIdea':
       button = 'dlgIdeaDelete'
       break;
-
     default:
       console.log(`Button not exist`);
   }
   document.getElementById(button).classList.add('active');
 }
 
-
 function getPeople(){
-
   const peopleColRef = collection(db, 'people');
   onSnapshot(peopleColRef, (querySnapshot) => { // will run once initially and each time there is a change in data in the collection*
   let people = [];
@@ -90,7 +78,6 @@ function getPeople(){
     const id = doc.id; //every `doc` object has a `id` property that holds the `_id` value from Firestore.
     people.push({id, ...data});
   });
-
   buildPeople(people);
   document.querySelector('.person').classList.add('selected')
   getIdeas(people[0].id)
@@ -103,15 +90,11 @@ function getPeople(){
 }
 
 function getIdeas(id){
-
-  //get an actual reference to the person document 
-  const personRef = doc(db, 'people', id);
-  //then run a query where the `person-id` property matches the reference for the person
-  const docs = query(
+  const personRef = doc(db, 'people', id);//get an actual reference to the person document 
+  const docs = query( //then run a query where the `person-id` property matches the reference for the person
     collection(db, 'gift-ideas'),
     where('person-id', '==', personRef)
     );
-    
   onSnapshot(docs, (querySnapshot) => {
     const gifts = []; //to hold all the gifts from the collection and to clear teh previous gifts
     querySnapshot.forEach((doc) => { 
@@ -121,16 +104,12 @@ function getIdeas(id){
     });
     buildGifts(gifts);
   })
-
 }
 
 function buildPeople(people){ 
-  //build the HTML
-  let ul = document.querySelector('ul.person-list');
-  //replace the old ul contents with the new.
-  ul.innerHTML = people.map(person=>{
-    const dob = `${months[person['birth-month']-1]} ${person['birth-day']}`;
-    //Use the number of the birth-month less 1 as the index for the months array
+  let ul = document.querySelector('ul.person-list'); //build the HTML
+  ul.innerHTML = people.map(person=>{ //replace the old ul contents with the new.
+    const dob = `${months[person['birth-month']-1]} ${person['birth-day']}`;//Use the number of the birth-month less 1 as the index for the months array
     return `<li data-id="${person.id}" class="person">
             <p class="name">${person.name}</p>
             <p class="dob">${dob}</p>
@@ -147,13 +126,11 @@ function buildPeople(people){
 }
 
 function buildGifts(gifts){
-  //build the HTML
-  let ul = document.querySelector('ul.idea-list');
-  //replace the old ul contents with the new.
-  if (gifts.length != 0) {
+ 
+  let ul = document.querySelector('ul.idea-list');  //build the HTML
+  if (gifts.length != 0) {   //replace the old ul contents with the new.
     ul.innerHTML = gifts.map(gift => {
       const ideaRef = doc(db, 'gift-ideas', gift.id)
-      
       return `<li class="idea" data-id="${gift.id}">
       <label for="chk-uniqueid">
       <input type="checkbox" class="chk-uniqueid" data-id="${gift.id}" ${gift.bought==true?'checked':''}/> Bought</label>
@@ -163,34 +140,28 @@ function buildGifts(gifts){
       <button class="btnDeleteIdea" >Delete Idea</button>
       </li>`
     }).join('');
-
-  document.querySelectorAll('.btnEditIdea').forEach((button) => {
-  // button.addEventListener('click', showOverlay);
-    button.addEventListener('click', (ev) => {
-      giftId = (ev.path[1].dataset.id)
-      showOverlay(ev, giftId)
-    });
-  })
-
-  document.querySelectorAll('.btnDeleteIdea').forEach((button) => {
-  // button.addEventListener('click', showOverlay);
-    button.addEventListener('click', (ev) => {
-      giftId = (ev.path[1].dataset.id)
-      showOverlay(ev, giftId)
-    });
-  })
-
-let checkboxes = document.querySelectorAll(".chk-uniqueid");
-checkboxes.forEach(checkbox => {
-  checkbox.addEventListener('change', function () {
-    const ideaRef = doc(db, 'gift-ideas', this.getAttribute("data-id"))
-    updateDoc(ideaRef, {
-    "bought": this.checked
+    document.querySelectorAll('.btnEditIdea').forEach((button) => {
+      button.addEventListener('click', (ev) => {
+        giftId = (ev.path[1].dataset.id)
+        showOverlay(ev, giftId)
+      });
     })
-  });
-})
-
-} else ul.innerHTML = `<h3 class="no-idea">There are no ideas added! click on "Add Idea" button to add one</h3>`
+    document.querySelectorAll('.btnDeleteIdea').forEach((button) => {
+      button.addEventListener('click', (ev) => {
+        giftId = (ev.path[1].dataset.id)
+        showOverlay(ev, giftId)
+      });
+    })
+    let checkboxes = document.querySelectorAll(".chk-uniqueid");
+    checkboxes.forEach(checkbox => {
+      checkbox.addEventListener('change', function () {
+        const ideaRef = doc(db, 'gift-ideas', this.getAttribute("data-id"))
+        updateDoc(ideaRef, {
+        "bought": this.checked
+        })
+      });
+    })
+  } else ul.innerHTML = `<h3 class="no-idea">There are no ideas added! click on "Add Idea" button to add one</h3>`
 }
 
 function selectPerson (ev) {
@@ -212,27 +183,19 @@ async function savePerson(ev){
     'birth-month': month,
     'birth-day': day
   };
-  
   try {
     const docRef = await addDoc(collection(db, 'people'), person );
     console.log('Document written with ID: ', docRef.id);
-    //0. add id to the person object
-    person.id = docRef.id
-    //1. clear the form fields 
-    document.getElementById('name').value = '';
+    person.id = docRef.id //0. add id to the person object
+    document.getElementById('name').value = '';  //1. clear the form fields 
     document.getElementById('month').value = '';
     document.getElementById('day').value = '';
-    //2. hide the dialog and the overlay
-    hideOverlay(ev);
+    hideOverlay(ev);//2. hide the dialog and the overlay
     //3. display a message to the user about success 
     // tellUser(`Person ${name} added to database`);
     // person.id = docRef.id;
-    //4. ADD the new HTML to the <ul> using the new object
-    // showPerson(person);
   } catch (err) {
     console.error('Error adding document: ', err);
-    //do you want to stay on the dialog?
-    //display a mesage to the user about the problem
   }
 }
 
@@ -249,26 +212,18 @@ async function saveIdea(ev){
     "person-id": selectedPersonRef,
     "bought": false
   };
-  
   try {
     const docRef = await addDoc(collection(db, 'gift-ideas'), idea );
     console.log('Document written with ID: ', docRef.id);
-    //0. add id to the idea object
-    idea.id = docRef.id
-    //1. clear the form fields 
-    document.getElementById('title').value = '';
+    idea.id = docRef.id //0. add id to the idea object
+    document.getElementById('title').value = ''; //1. clear the form fields 
     document.getElementById('location').value = '';
-    //2. hide the dialog and the overlay
-    hideOverlay(ev);
+    hideOverlay(ev);  //2. hide the dialog and the overlay
     //3. display a message to the user about success 
     // tellUser(`Person ${name} added to database`);
     // person.id = docRef.id;
-    //4. ADD the new HTML to the <ul> using the new object
-    // showIdea(idea);
   } catch (err) {
     console.error('Error adding document: ', err);
-    //do you want to stay on the dialog?
-    //display a mesage to the user about the problem
   }
 }
 
@@ -276,18 +231,15 @@ function deletePerson (ev) {
   const docRef = doc(db, 'people', document.querySelector(".selected").getAttribute("data-id"))
   deleteDoc(docRef)
   hideOverlay(ev)
-  // showPerson()
 }
 
 function editPerson (ev) {
-  // ev.preventDefault();
   const docRef = doc(db, 'people', document.querySelector(".selected").getAttribute("data-id"))
   updateDoc(docRef, {
     'name': document.getElementById('editName').value,
     'birth-month': document.getElementById('editMonth').value,
     'birth-day': document.getElementById('editDay').value
-  })
-  .then(() => {
+  }).then(() => {
     document.getElementById('editName').value = '';
     document.getElementById('editMonth').value = '';
     document.getElementById('editDay').value = '';
@@ -302,21 +254,13 @@ function deleteIdea (ev) {
 }
 
 function editIdea (ev) {
-
-
-  console.log(giftId)
-  // const docRef = doc(db, 'people', document.querySelector(".selected").getAttribute("data-id"))
-  // updateDoc(docRef, {
-  //   'name': document.getElementById('editName').value,
-  //   'birth-month': document.getElementById('editMonth').value,
-  //   'birth-day': document.getElementById('editDay').value
-  // })
-  // .then(() => {
-  //   document.getElementById('editName').value = '';
-  //   document.getElementById('editMonth').value = '';
-  //   document.getElementById('editDay').value = '';
-  //   hideOverlay(ev)
-  // })
-
+  const docRef = doc(db, 'gift-ideas', giftId)
+  updateDoc(docRef, {
+    "idea" : document.getElementById('editTitle').value,
+    "location" : document.getElementById('editLocation').value
+  }).then(() => {
+    document.getElementById('editTitle').value = '';
+    document.getElementById('editLocation').value = '';
+    hideOverlay(ev);
+  })
 }
-
